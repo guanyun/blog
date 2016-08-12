4396e05f5e14ff1e46a4b137aa359c562a2119b5
feipinghuang <feiping.huang@thexstore.cn>
2016-08-12 14:55:05 +0800

    re init repo

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+require 'mina/bundler'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :user, 'ubuntu'
+set :domain, 'thexstore.cn'
+set :deploy_to, '/var/www/blog'
+set :repository, 'git@github.com:h0mebrew/blog.git'
+set :branch, 'master'
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+desc 'Deploys the current version to the server.'
+task deploy: :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    queue echo_cmd "#{bundle_prefix} jekyll build"
+  end
+end
